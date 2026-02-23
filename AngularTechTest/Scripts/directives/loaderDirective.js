e20f2f415166461dd4e48f10c99d41516436ae56
Mouazalkhawam <afrot2002@gmail.com>
2026-02-23 21:42:54 +0200

    Revert "Add vm pattern and toast notifications"
    This reverts commit 6366d9d40d93a66ab1d7636462048a72e97f8ca5.

--- AngularTechTest/Scripts/directives/loaderDirective.js
+++ AngularTechTest/Scripts/directives/loaderDirective.js
@@ -1,48 +1,31 @@
-﻿(function () {
-    try {
-        angular.module('MyApp')
-            .directive('loader', function () {
-                return {
-                    restrict: 'A',
-                    scope: {
-                        isLoading: '=loader',
-                        clickAction: '&'
-                    },
-                    link: function (scope, element, attrs) {
-                        var originalText = element.html();
+﻿angular.module('MyApp').directive('loader', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var originalText = element.html();
 
-                        element.bind('click', function () {
-                            if (!scope.isLoading) {
-                                scope.$apply(function () {
-                                    scope.isLoading = true;
-                                });
+            element.bind('click', function () {
+                if (!scope[attrs.loader]) {
+                    scope.$apply(function () {
+                        scope[attrs.loader] = true;
+                    });
 
-                                element.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Saving...');
-                                element.prop('disabled', true);
+                    element.html('<span class="spinner-border"></span> Saving...');
+                    element.prop('disabled', true);
 
-                                var result = scope.clickAction();
+                    var result = scope.$eval(attrs.ngClick);
 
-                                if (result && result.finally) {
-                                    result.finally(function () {
-                                        scope.$apply(function () {
-                                            scope.isLoading = false;
-                                        });
-                                        element.html(originalText);
-                                        element.prop('disabled', false);
-                                    });
-                                } else {
-                                    scope.$apply(function () {
-                                        scope.isLoading = false;
-                                    });
-                                    element.html(originalText);
-                                    element.prop('disabled', false);
-                                }
-                            }
+                    if (result && result.finally) {
+                        result.finally(function () {
+                            scope.$apply(function () {
+                                scope[attrs.loader] = false;
+                            });
+                            element.html(originalText);
+                            element.prop('disabled', false);
                         });
                     }
-                };
+                }
             });
-    } catch (e) {
-        console.log('Loader directive waiting for module...');
-    }
-})();
+        }
+    };
+});
